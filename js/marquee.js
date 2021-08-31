d400a6b4c00c88f6b94e2d62da41dada32714369
Darien-Cepani <darien.cepani42@gmail.com>
2021-08-31 23:55:14 +0200

    Fixed a couple issues

--- js/marquee.js
+++ js/marquee.js
@@ -7,20 +7,20 @@ const marquee3 = document.querySelector("ul.line3");
 // Line 1
 root.style.setProperty("--marquee-elements", marquee1.children.length);
 
-for(let i=0; i<marqueeElementsDisplayed+10; i++) {
+for(let i=0; i<marqueeElementsDisplayed; i++) {
   marquee1.appendChild(marquee1.children[i].cloneNode(true));
 }
 
 // Line 2
 root.style.setProperty("--marquee-elements", marquee2.children.length);
 
-for(let i=0; i<marqueeElementsDisplayed+10; i++) {
+for(let i=0; i<marqueeElementsDisplayed; i++) {
   marquee2.appendChild(marquee2.children[i].cloneNode(true));
 }
 
 // Line 3
 root.style.setProperty("--marquee-elements", marquee3.children.length);
 
-for(let i=0; i<marqueeElementsDisplayed+10; i++) {
+for(let i=0; i<marqueeElementsDisplayed; i++) {
   marquee3.appendChild(marquee3.children[i].cloneNode(true));
 }
